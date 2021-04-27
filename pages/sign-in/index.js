686311a89300ba8caa1d76681cc15dc82810ab09
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-04-27 17:21:16 +0200

    Add Sign in component

---
+++ pages/sign-in/index.js
@@ -0,0 +1,88 @@
+import { useClerk } from '@clerk/clerk-react'
+import { useRouter } from 'next/router'
+import { useForm } from 'react-hook-form'
+
+const SignIn = () => {
+  const { client } = useClerk();
+  const { signInAttempt } = client;
+  const { register, handleSubmit } = useForm();
+  const router = useRouter();
+
+  const onSubmit = ({ identifier, password }) => {
+    try {
+      signInAttempt.create({ identifier });
+      signInAttempt.attemptFactorOne({ strategy: 'password', password });
+      router.push('/');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="flex flex-col min-h-screen py-24 bg-gray-50 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img className="w-auto h-12 mx-auto" src="./clerk.svg" alt="Clerk" />
+        <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">
+          Sign in
+        </h2>
+      </div>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  {...register('identifier')}
+                  className="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  {...register('password')}
+                  className="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
